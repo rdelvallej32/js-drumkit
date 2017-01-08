122ae741f8a1ae5098c61b0a9601a82158e7fa19
rdelvallej32 <rdelvallej@gmail.com>
2017-01-08 09:26:22 -0500

    Fix keycodes for the audio

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 window.addEventListener('keydown', function(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key]="${e.keyCode}"`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if (!audio) {
         return;
     }
